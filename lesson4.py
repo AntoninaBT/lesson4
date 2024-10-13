bba09c3bc18cda0621c6d64a438e9218c4cf01ed
AntoninaBT <gorbaa533@gmail.com>
2024-10-13 17:35:34 +0300

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,6 @@
+# Teplova// 4 variables. I need to output the name of the course, total tasks, hours and an average time. I used "" when I printed the text data, I didn't use "" when I wrote figures (the variables that consist figures) and variables in the "print"
+tasks = 12
+hours = 1.5
+name = "Python"
+time = hours / tasks
+print("course:",name,", total tasks:",tasks,", total hours:", hours, ", average time:", time)
